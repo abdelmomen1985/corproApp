051cb401a98bd96ba854941f56154d2ebe3671cf
Andrew Amgad <macbookair@Macbooks-MacBook-Air.local>
2020-07-06 13:52:27 +0200

    Request card fix

--- src/components/Requests/RequestCard.tsx
+++ src/components/Requests/RequestCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/react';
+import { formatDate } from '../../helpers';
 
 export default function RequestCard(props: any) {
 
     const { request } = props;
-    const { content, status } = request;
+    const { content, status, created_at } = request;
 
     let statusText: string = ""
 
@@ -15,6 +16,7 @@ export default function RequestCard(props: any) {
     return (
         <IonCard style={{ cursor: 'pointer' }} onClick={props.onClick}>
             <IonCardHeader>
+                <IonCardSubtitle>{formatDate(created_at)}</IonCardSubtitle>
                 <IonCardTitle>{statusText}</IonCardTitle>
             </IonCardHeader>
 
