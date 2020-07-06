5087b62421df0d46637bdb110152d2d7c61d47d6
fireb1003 <34112797+fireb1003@users.noreply.github.com>
2020-07-06 14:02:13 +0200

    scanner init

---
+++ README.md
@@ -0,0 +1,4 @@
+## Adding barcode Scanner
+
+- https://ionicframework.com/docs/native/barcode-scanner
+- https://ionicframework.com/docs/native/community#react
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,21 +6,28 @@ import {
   IonTitle,
   IonToolbar,
   IonMenuButton,
+  IonButton,
 } from "@ionic/react";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
 
-import {setLanguage, strings} from '../localization/localization'; 
+import { setLanguage, strings } from "../localization/localization";
 
 const Home: React.FC = () => {
+  const openScanner = async () => {
+    const data = await BarcodeScanner.scan();
+    console.log(`Barcode data: ${data.text}`);
+  };
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonMenuButton slot="start"/>
+          <IonMenuButton slot="start" />
           <IonTitle>{strings.main.home}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <button onClick={setLanguage}>Change Language</button>
+        <IonButton onClick={openScanner}>Scan</IonButton>
       </IonContent>
     </IonPage>
   );
